15e110bc18672044ed3f2a8e94c718459a8514f6
Michelle Garcia <thelionpear@gmail.com>
2018-11-05 11:35:35 -0700

    navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,13 @@
 import React from "react"; 
+import { NavLink } from "react-router-dom"; 
 
 const Navigation = () => {
   return(
-    <div>Navigation</div> 
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/About">About</NavLink>
+      <NavLink to="/Contact">Contact</NavLink>
+    </div> 
   );
 };
 
