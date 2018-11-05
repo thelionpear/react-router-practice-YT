3aa00737f549002279e3789470b605c6cec2e0e2
Michelle Garcia <thelionpear@gmail.com>
2018-11-05 11:14:55 -0700

    initial example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'; 
+//Router takes in the pathname you want and the component 
+
+const NewRoute = () => {
+  return (
+    <div>
+      <p>New Route</p>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter> 
+        <Route path="/new" component={NewRoute}/>
+      </BrowserRouter> 
     );
   }
 }
