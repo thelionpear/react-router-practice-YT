958e472671d2edd81e390b499acb7f65e3a21341
Michelle Garcia <thelionpear@gmail.com>
2018-11-05 11:29:58 -0700

    dummy components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'; 
+import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
+import Home from "./components/Home"; 
+import About from "./components/About"; 
+import Contact from "./components/Contact"; 
+import Error from "./components/Error";
+import Navigation from "./components/Navigation"; 
 //Router takes in the pathname you want and the component 
-
-const NewRoute = () => {
-  return (
-    <div>
-      <p>New Route</p>
-    </div>
-  );
-}
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter> 
-        <Route path="/new" component={NewRoute}/>
+        <div> 
+          <Navigation /> 
+          <Switch>
+            <Route path="/" component={Home} exact/> 
+            <Route path="/About" component={About} /> 
+            <Route path="/Contact" component={Contact} /> 
+            <Route component={Error} /> 
+          </Switch>
+        </div> 
       </BrowserRouter> 
     );
   }
